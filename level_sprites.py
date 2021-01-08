08dee8840b855596ad53584cc7d02d1a3896e69c
bluejamm <77124738+bluejamm@users.noreply.github.com>
2021-01-07 20:23:45 -0800

    Level-Framework-Progress

--- level_sprites.py
+++ level_sprites.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from environment_sprite import Environment_Sprite
 level_one = [
 # Sprite("Grass_Top_1.png", 0, 0, True),
@@ -6,4 +7,14 @@ level_one = [
 for number in range(0, 32):
     level_one.append(Environment_Sprite("Grass_Green_Center_Center.png",number ,13 , True))
 for number in range(0, 32):
+=======
+from environment_sprite import Environment_Sprite
+level_one = [
+# Sprite("Grass_Top_1.png", 0, 0, True),
+# Sprite("Grass_Top_1.png", 0, 1, True)
+]
+for number in range(0, 32):
+    level_one.append(Environment_Sprite("Grass_Green_Center_Center.png",number ,13 , True))
+for number in range(0, 32):
+>>>>>>> Stashed changes
     level_one.append(Environment_Sprite("Grass_Green_Top_Center.png", number, 12, True))
