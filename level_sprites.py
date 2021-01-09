cc91e9247250a58f1afb270c4d8e2dcdf3d4065e
Opal-M <raspberryopal@gmail.com>
2021-01-09 14:01:19 -0800

    capped the rest of sprites, moved drawing code

--- level_sprites.py
+++ level_sprites.py
@@ -15,11 +15,13 @@ def draw_capped_strip(x,y,length,start,middle,end):
     level_one.append(Environment_Sprite(start, x, y, True))
     for number in range(1,length):
         level_one.append(Environment_Sprite(middle, x+number, y, True))
-    level_one.append(Environment_Sprite(end, x+length, y, True))
-draw_strip(0,13,32,"Grass_Green_Center_Center.png")
-draw_strip(0,12,32,"Grass_Green_Top_Center.png")
-draw_strip(10,9,10,"Brick_Center_Center.png")
-draw_strip(0,14,32,"Grass_Green_Center_Center.png")
-draw_strip(0,15,32,"Grass_Green_Center_Center.png")
-draw_capped_strip(15,6,8,"Brick_Center_Left.png","Brick_Center_Center.png","Brick_Center_Right.png")
-draw_capped_strip(8,3,5,"Brick_Center_Left.png","Brick_Center_Center.png","Brick_Center_Right.png")
+    level_one.append(Environment_Sprite(end, x+(length-1), y, True))
+def draw_level(number):
+    if number == 1:
+        draw_capped_strip(0,13,32,"Grass_Green_Center_Left.png","Grass_Green_Center_Center.png","Grass_Green_Center_Right.png")
+        draw_capped_strip(0,12,32,"Grass_Green_Top_Left.png","Grass_Green_Top_Center.png","Grass_Green_Top_Right.png")
+        draw_capped_strip(10,9,10,"Brick_Center_Left.png","Brick_Center_Center.png","Brick_Center_Right.png")
+        draw_capped_strip(0,14,32,"Grass_Green_Center_Left.png","Grass_Green_Center_Center.png","Grass_Green_Center_Right.png")
+        draw_capped_strip(0,15,32,"Grass_Green_Center_Left.png","Grass_Green_Center_Center.png","Grass_Green_Center_Right.png")
+        draw_capped_strip(15,6,8,"Brick_Center_Left.png","Brick_Center_Center.png","Brick_Center_Right.png")
+        draw_capped_strip(8,2,5,"Brick_Center_Left.png","Brick_Center_Center.png","Brick_Center_Right.png")
